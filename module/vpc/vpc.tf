40ee0326cdc96f7b2fe2796403a4d5a955b89923
FRAFUCHI <alphasuno214@gmail.com>
2022-03-28 21:14:30 +0900

    refact: ㅅㅜ정

--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "this" {
-    cidr_block = "10.0.0.0/16" 
+    cidr_block = "10.0.0.0/24" 
     enable_dns_hostnames = true 
     tags =  { 
       Name = "${var.project_name}-vpc"
@@ -8,7 +8,7 @@ resource "aws_vpc" "this" {
 
 resource "aws_subnet" "public_a" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "" 
+    cidr_block = "10.0.0.128/26" 
     availability_zone = "${var.region_a}" 
     map_public_ip_on_launch = false 
     tags = { 
@@ -19,8 +19,8 @@ resource "aws_subnet" "public_a" {
 
 resource "aws_subnet" "public_c" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "${var.subnet_pub_b_cidr}" 
-    availability_zone = "ap-northeast-2c" 
+    cidr_block = "10.0.0.192/26" 
+    availability_zone = "${var.region_c}" 
     map_public_ip_on_launch = false 
     tags = { 
         Name = "${var.project_name}-pub-c"
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_c" {
 
 resource "aws_subnet" "private_a" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "${var.subnet_pri_a_cidr}" 
+    cidr_block = "10.0.0.0/26" 
     availability_zone = "${var.region_a}" 
     map_public_ip_on_launch = false 
     tags = { 
@@ -40,7 +40,7 @@ resource "aws_subnet" "private_a" {
 
 resource "aws_subnet" "private_c" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "" 
+    cidr_block = "10.0.0.64/26" 
     availability_zone = "${var.region_c}"  
     map_public_ip_on_launch = false 
     tags = { 
@@ -50,7 +50,7 @@ resource "aws_subnet" "private_c" {
 
 resource "aws_subnet" "private_db_a" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "" 
+    cidr_block = "10.0.0.256/26" 
     availability_zone = "${var.region_a}" 
     map_public_ip_on_launch = false 
     tags = { 
@@ -60,7 +60,7 @@ resource "aws_subnet" "private_db_a" {
 
 resource "aws_subnet" "private_db_b" {
     vpc_id = aws_vpc.this.id 
-    cidr_block = "" 
+    cidr_block = "10.0.1.0/26"  
     availability_zone = "${var.region_c}"  
     map_public_ip_on_launch = false 
     tags = { 
